1f4648909f0002714040610cae69ab6682c986c0
Elliot Agro <elliotagro83@gmail.com>
2015-01-14 12:28:41 +0000

    added defaults

--- process/app.js
+++ process/app.js
@@ -11,8 +11,13 @@ var scrap   = require('scrap');
 exports.getData = function(settings) {
 
     // this is the object saved from your the /input portion of the slab.
-    var searchTerm  = settings.searchTerm || 'lab';
-    var siteUrl     = settings.siteUrl || 'http://www.dailymail.co.uk';
+    var searchTerm  = 'lab';
+    var siteUrl     = 'http://www.dailymail.co.uk';
+
+    if(settings){
+        searchTerm  = settings.searchTerm;
+        siteUrl     = settings.siteUrl;
+    }
 
     // Slabs works on a promise system - for this we use the excellent 'Q' library.
     var deferred = Q.defer();
